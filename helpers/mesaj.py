74ff12e69bc87b7f117ff534f5d900eed0f8139b
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-10-31 04:48:07 +0400

    update mesaj.py

--- helpers/mesaj.py
+++ helpers/mesaj.py
@@ -1,1 +1,72 @@
+salam = (
+"Salam",
+"Salam necəsən",
+"Salam Ürəyimmm ❤️",
+"Salamdaa",
+"Uşş canım salam",
+"Salam Cənab 🫶",
+"Salam gözəl insan",
+)
+
+necesen = (
+"Saol",
+"Həkimsən ?",
+"Ə belədana 😂",
+"Sənə nə",
+"İyyim aşkım sen ?",
+"yaxşı olmağa çalışıram",
+"Mən başımı buraxe sən necəsən 😂",
+"Həyatdıda yaşadır",
+)
+
+sagol = (
+"Salam Sağol",
+"Hara gedsən",
+"Yatıram demə🥲",
+"Sağolunnn yenə gözləyəriyy🙈",
+"Uşş balam Sağol",
+"Sağol canım benim 🫶",
+"Sağol Kişi 🔥",
+"Sağol uğurlar",
+)
+
+getdim = (
+"Hara",
+"Getmə",
+"Get bidə gəlmə",
+)
+
+geldim = (
+"Xoş Gəldin ❤️",
+"Xoş gəldin nəfəs",
+"Xoş gəldin necəsən",
+)
+
+teamabasov = (
+"O mənim sahibimdir",
+"O burda olmasada mən sizinləyəm",
+"Onun ürəyi sizinlədi🫶",
+"O indi belkem yatır. O gecə oyaq, gündüz yatır 😴",
+)
+
+ban = (
+"Vəhşii",
+"Kimi ban edəcəksən",
+"Vəhşi Panteramm kimə ban atdın",
+"Havada ban kokusu var",
+)
+
+fds = (
+"Groupda qız olar utanmırsan",
+"Utanmırsan",
+"Bu qroupda bele sohbet etme",
+"Yaxşı deyil",
+)
+
+hardasan = (
+"Mən hər zaman burdayam",
+"Başqa groupda idim",
+"Sən hardasan",
+)
+
 
